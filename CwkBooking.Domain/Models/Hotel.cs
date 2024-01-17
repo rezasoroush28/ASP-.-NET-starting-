0ad1e9ad33455360d2d96dd4b24139fa67741b81
reza <rezsoroush28@gmail.com>
2024-01-17 06:06:49 +0330

    starting

---
+++ CwkBooking.Domain/Models/Hotel.cs
@@ -0,0 +1,9 @@
+namespace CwkBooking.Domain.Models
+{
+    public class Hotel
+    {
+        public int HotelId { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+    }
+}
