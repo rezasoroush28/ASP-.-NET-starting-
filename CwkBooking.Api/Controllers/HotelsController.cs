36fc16c22e0309e9dfb26958dce9723d6cb756a5
reza <rezsoroush28@gmail.com>
2024-01-22 03:25:18 +0330

    CRUD used in my api

--- CwkBooking.Api/Controllers/HotelsController.cs
+++ CwkBooking.Api/Controllers/HotelsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using CwkBooking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CwkBooking.Api.Controllers
@@ -11,9 +14,85 @@ namespace CwkBooking.Api.Controllers
             
         }
         [HttpGet]
-        public IActionResult GetRooms()
+        public IActionResult ShowAllHotels()
         {
-            return Ok("hello");
+            var hotels = GetHotels();
+            return Ok(GetHotels());
+        }
+        
+        
+
+
+        [Route("{name}")]
+        [HttpGet]
+        public IActionResult GetHotelbyName(string name)
+        {
+            var hotels = GetHotels();
+            var hotel = hotels.FirstOrDefault(h => h.Name == name);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return Ok(hotel);
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateHotel([FromBody] Hotel hotel)
+        {
+            var hotels = GetHotels();
+            hotels.Add(hotel);
+            return CreatedAtAction(nameof(GetHotelbyName), new { name = hotel.Name }, hotel);
+        }
+
+        [HttpPut]
+        [Route("{name}")]
+
+        public IActionResult UpdateHotel([FromBody] Hotel toBeUpdatedHotel , string name)
+        {
+            var hotels = GetHotels();
+            var oldHotel = hotels.FirstOrDefault(h => h.Name == name);
+            if (oldHotel == null)
+            {
+                return NotFound("no hotels with this name found");
+
+            }
+            hotels.Remove(oldHotel);
+            hotels.Add(toBeUpdatedHotel);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{name}")]
+        public IActionResult DeleteHotel(string name)
+        {
+            var hotels = GetHotels();
+            var toBeDeletedHotel = hotels.FirstOrDefault(h => h.Name == name);
+            if (toBeDeletedHotel == null)
+            {
+                return NotFound("no hotels with this name found");
+
+            }
+
+            hotels.Remove(toBeDeletedHotel);
+            return NoContent();
+        }
+        
+        
+        private List<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                new Hotel
+                {
+                    HotelId = 1,
+                    Name = "Ghasr",
+                    Stars = 5,
+                    Address = "tehran - somwhere",
+                    City = "tehran",
+                    Country = "iran",
+                }
+            };
         }
     }
 }
